d401062ba2ea6738cc323f0ed85a0b2238ccc1f9
Dalton Overmyer <dalton@raptorpowersystems.com>
2022-01-06 11:43:53 -0600

    added error handling

--- src/main.cpp
+++ src/main.cpp
@@ -12,25 +12,28 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  SDL_Window *window = SDL_CreateWindow(APP_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
-
-  std::shared_ptr<SDL_Renderer> renderer(SDL_CreateRenderer(window, -1, 0), [](SDL_Renderer *p) { SDL_DestroyRenderer(p); });
-
+  std::shared_ptr<SDL_Window> window(SDL_CreateWindow(APP_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE), [](SDL_Renderer *p) { SDL_DestroyWindow(p); });
+  if(!window){
+    std::cout << "Failed to create window: " << SDL_GetError() << "\n";
+    return 1;
+  }
+  
+  std::shared_ptr<SDL_Renderer> renderer(SDL_CreateRenderer(window.get(), -1, 0), [](SDL_Renderer *p) { SDL_DestroyRenderer(p); });
+    if(!renderer){
+    std::cout << "Failed to create renderer: " << SDL_GetError() << "\n";
+    return 1;
+  }
   Sandpile world(renderer, INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT, WORLD_WIDTH, WORLD_HEIGHT);
 
-  int index = 0;
   for (int i = 0; i < world.width(); i++) {
     for (int j = 0; j < world.height(); j++) {
       world.add_sand(i, j, 10);
-      index++;
     }
   }
 
   world.start(6);
-
   world.join();
 
-  SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
 }
